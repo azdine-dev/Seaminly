b9e7af264df40ae3449f5bd6f5a78e62afa5f369
Azeddine BENHLIMA <azeddinebenhalima@gmail.com>
2020-06-27 13:30:38 +0100

    add Student Controller

--- src/main/java/com/azdine/dev/SeaminlyApplication.java
+++ src/main/java/com/azdine/dev/SeaminlyApplication.java
@@ -1,4 +1,4 @@
-package com.azdine.dev.seaminly;
+package com.azdine.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/azdine/dev/entities/Student.java
+++ src/main/java/com/azdine/dev/entities/Student.java
@@ -1,20 +1,65 @@
 package com.azdine.dev.entities;
 
-import lombok.Data;
-import lombok.Getter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 @Entity
-@Data
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private String firstName;
+    @NotNull
     private String lastName;
+    @Email
+    private String email;
+
     private Date dateNaissance;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
